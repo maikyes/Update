e095a4859c4714b57c4b99469530a3e8d8155c37
maikyes <80900724+maikyes@users.noreply.github.com>
2021-05-21 17:19:41 -0400

    Update status.bat

--- status.bat
+++ status.bat
@@ -1,4 +1,4 @@
-set new1= 
-set new2= SoftAim: Safe
-set new3= FNSS Server: Making News Script
-set new4=
+set new1= SoftAim: Safe
+set new2= FNSS Server: Making News Script
+set new3= 
+set new4= Please Support me Typing [ s ]
