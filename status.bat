13bfcce86be5dcec3aa344dbd16fc2d66d953f8e
maikyes <80900724+maikyes@users.noreply.github.com>
2021-05-25 22:51:29 -0400

    Update status.bat

--- status.bat
+++ status.bat
@@ -1,4 +1,4 @@
-set new1= SoftAim: Maybe Risky for Public
+set new1= SoftAim: Updating bc has Virus
 set new2= FNSS Server: Disable Microsoft Defender
 set new3= Please Support me Typing [ s ]
 set new4= To add more Content!
