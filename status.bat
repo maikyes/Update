2c141f9d7a281ac946db877f062097714e46cd8d
maikyes <80900724+maikyes@users.noreply.github.com>
2021-05-28 02:41:54 -0400

    Update status.bat

--- status.bat
+++ status.bat
@@ -1,4 +1,4 @@
-set new1= SoftAim: Updating bc has Virus
-set new2= FNSS Server: Disable Microsoft Defender
+set new1= SoftAim: Is safe from virus
+set new2= FNSS Server: Will be close in 5/31/2021
 set new3= Please Support me Typing [ s ]
 set new4= To add more Content!
