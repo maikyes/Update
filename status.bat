92bbb95e5ec1ffd40980cae2a872175a1c93e286
maikyes <80900724+maikyes@users.noreply.github.com>
2022-05-05 12:10:03 -0400

    Update status.bat

--- status.bat
+++ status.bat
@@ -1,4 +1,4 @@
-set new1= Server shutdown 11/25/2021
-set new2= Server shutdown 11/25/2021
-set new3= Server shutdown 11/25/2021
-set new4= Server shutdown 11/25/2021
+set new1= Estas bien puta beto
+set new2= 
+set new3= 
+set new4= 
